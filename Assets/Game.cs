96164053730421b814ee9c0b17584846b3a02288
Drovyng <TrueDrovyng@gmail.com>
2023-05-25 18:39:57 +0300

    v1.6.2

--- Assets/Game.cs
+++ Assets/Game.cs
@@ -131,7 +131,7 @@ public class Game : MonoBehaviour
     }
     public void ScreenTouch()
     {
-        if (menu.opened || gameCanStart > 0)
+        if (menu.opened || gameCanStart > 0 || !adShown)
         {
             return;
         }
